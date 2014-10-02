1a0322e80cb7990f1a441f497d4a2ca5a049c387
kshim <kyujin.shim@macquarie.com>
2014-10-02 17:10:13 +0800

    asdf

---
+++ src/main/scala/CF467C.scala
@@ -0,0 +1,18 @@
+import scala.collection.mutable
+
+/**
+ * Created by lurker on 2014. 8. 15..
+ * http://codeforces.com/problemset/problem/467/C
+ */
+object CF467C extends App {
+  @inline def tokenizeLine = new java.util.StringTokenizer(readLine)
+  def readInts(n: Int) = {
+    val tl = tokenizeLine; Array.fill(n)(tl.nextToken.toInt)
+  }
+
+  val Array(n,m,k) = readInts(3)
+  val p = readInts(n)
+
+  val d = Array.ofDim[Long](n, k)
+  d.f
+}
